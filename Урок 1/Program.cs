e12ae9cefac95ff0bffb2ba75e1bd4b91d7725be
MaryQueen1 <108994391+MaryQueen1@users.noreply.github.com>
2022-08-06 14:50:37 +0300

    Delete

--- Урок 1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Write("Enter your number");
-double a;
-do Console.Write("a = ");
-while (!double.TryParse(Console.ReadLine(), out a));
-Console.Write("Ваше число в квадрате = ");
-Console.Write(a * a);
