cd85d74754792d7e6864c041d092fcb30411b69f
Mary <maryqueenk1@gmail.com>
2022-08-06 14:58:11 +0300

    Add Ex02

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Enter your number");
+﻿// Напишите программу, которая на входе принимает число и выдыет его квадрат (число умноженное на часо себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Enter your number");
 double a;
 do Console.Write("a = ");
 while (!double.TryParse(Console.ReadLine(), out a));
