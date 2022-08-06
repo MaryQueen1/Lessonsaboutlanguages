aff366ae83c51f5383be2128dcccfc2ba447b7e9
Mary <maryqueenk1@gmail.com>
2022-08-06 15:15:37 +0300

    add ex02

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -4,3 +4,25 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
+
+Console.Write("Enter number");
+double a;
+do Console.Write("a = ");
+
+while (!double.TryParse(Console.ReadLine(), out a));
+
+Console.Write("Enter number");
+double b;
+do Console.Write("b = ");
+
+while (!double.TryParse(Console.ReadLine(), out b));
+
+if(b == a * a)
+{
+    Console.Write("ИСТИНА");
+}
+
+else
+{
+        Console.Write("ЛОЖЬ");
+}
